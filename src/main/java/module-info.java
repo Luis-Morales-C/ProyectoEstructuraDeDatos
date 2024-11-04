16a0bc99a986820c4cfea35f28faf6560f54d4c2
JuanEstebanVictoria <juane.victorial@uqvirtual.edu.co>
2024-11-04 16:11:47 -0500

    1

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.estructuradatospro {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.estructuradatospro to javafx.fxml;
-    exports org.example.estructuradatospro;
-}

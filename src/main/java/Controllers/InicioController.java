21b1195b1de9849a68fcad8d926df34327babd87
anaIter <tatianamosqueraiter182@gmail.com>
2024-11-15 20:35:29 -0500

    Controladores y esqueleto

--- src/main/java/Controllers/InicioController.java
+++ src/main/java/Controllers/InicioController.java
@@ -1,4 +1,41 @@
 package Controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
 
 public class InicioController {
+
+        @FXML
+        private Button btnGetIntoLogin;
+
+        @FXML
+        private Hyperlink hyperlinkManagerLogin;
+
+        @FXML
+        private Hyperlink hyperlinkRegisterLogin;
+
+        @FXML
+        private TextField txtIdLogin;
+
+        @FXML
+        private TextField txtPasswordLogin;
+
+        @FXML
+        void clickedGetIntoLogin(ActionEvent event) {
+
+        }
+
+        @FXML
+        void clickedManagerLogin(ActionEvent event) {
+
+        }
+
+        @FXML
+        void clickedRegisterLogin(ActionEvent event) {
+
+        }
+
+
 }
--- src/main/java/Controllers/RegisterContoller.java
+++ src/main/java/Controllers/RegisterContoller.java
@@ -1,4 +1,42 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class RegisterContoller {
+
+        @FXML
+        private Button btnRegisterRegister;
+
+        @FXML
+        private ComboBox<?> cBoxNotificationRegister;
+
+        @FXML
+        private TextField txtCountryRegister;
+
+        @FXML
+        private TextField txtIdRegister;
+
+        @FXML
+        private TextField txtMailRegister;
+
+        @FXML
+        private TextField txtNameRegister;
+
+        @FXML
+        private TextField txtNewPasswordRegister;
+
+        @FXML
+        private TextField txtPhoneNumberRegister;
+
+        @FXML
+        void clickedRegisterRegister(ActionEvent event) {
+
+        }
+
+
+
 }

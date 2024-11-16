4d0afa75572f4e9923827190132a1a3ad5d4f554
anaIter <tatianamosqueraiter182@gmail.com>
2024-11-16 16:27:37 -0500

    Controladores y appPrincipal

---
+++ src/main/java/Controllers/AppController.java
@@ -0,0 +1,42 @@
+package Controllers;
+
+import Model.Activity;
+import Model.Tool;
+import Model.User;
+public enum AppController {
+
+    INSTANCE();
+    private final Tool herramienta;
+    private User usuario;
+    private Process proceso;
+    private Activity actividad;
+
+    AppController() {
+        this.actividad = null;
+        this.proceso = null;
+        this.usuario = null;
+        herramienta = new Tool("ADAN");
+    }
+
+    public Tool getHerramienta() {
+        return herramienta;
+    }
+    public void setProcesoActual(Process proceso){
+        this.proceso = proceso;
+    }
+    public Process getProcesoActual(){
+        return proceso;
+    }
+    public void setActividadActual(Activity actividad){
+        this.actividad = actividad;
+    }
+    public Activity getActividadActual(){
+        return actividad;
+    }
+    public void setUsuarioActual(User usuario){
+        this.usuario = usuario;
+    }
+    public User getUsuarioActual(){
+        return usuario;
+    }
+}
--- src/main/java/Controllers/InicioController.java
+++ src/main/java/Controllers/InicioController.java
@@ -1,15 +1,19 @@
 package Controllers;
+import App.AppPrincipal;
 import Model.Tool;
+import Utils.ShowMessage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
-
+import Exception.UserDoesntExistException;
+import Exception.IncompleteDataException;
+import  static Controllers.AppController.INSTANCE;
 public class InicioController {
 
        //corregir cuando este el appController
-       // Tool tool =  Instance.getTool;
+        Tool tool =  INSTANCE.getHerramienta();
         @FXML
         private Button btnGetIntoLogin;
 
@@ -25,15 +29,33 @@ public class InicioController {
         @FXML
         private TextField txtPasswordLogin;
 
+        private AppPrincipal appPrincipal;
         @FXML
         void clickedGetIntoLogin(ActionEvent event) {
-                String email = "";
-                String password = "";
+            String email = "";
+            String password = "";
 
-                email = txtIdLogin.getText();
-                password = txtPasswordLogin.getText();
+            email = txtIdLogin.getText();
+            password = txtPasswordLogin.getText();
+            try {
+                if (datosValidos(email, password)) {
+                    if (tool.searchUser(email).getPassword().equals(password)) {
+                        INSTANCE.setUsuarioActual(tool.searchUser(email));
 
+                        AppPrincipal.mostrarVentanaProcesosAdmin();
+                    } else {
+                        throw new UserDoesntExistException();
+                    }
+                } else {
+                    throw new IncompleteDataException();
+                }
+            } catch (IncompleteDataException e) {
+                ShowMessage.mostrarMensaje("Notificacion Inicio sesion", "Datos Incompletos", "Debe ingresar los datos correctamente, despues de 3 intentos se bloqueara el usuario");
+            } catch (UserDoesntExistException e) {
+                ShowMessage.mostrarMensaje("Notificacion Inicio sesion", "Usuario no existe", "El usuario no existe o la contrasenia es incorrecta");
+            }
         }
+
 
         @FXML
         void clickedManagerLogin(ActionEvent event) {
@@ -45,7 +67,11 @@ public class InicioController {
 
         }
 
-        private boolean datosValidos(String userName, String password) {
+    public void setAplicacion(AppPrincipal principal) {
+            this.appPrincipal = principal;
+    }
+
+    private boolean datosValidos(String userName, String password) {
                 return !userName.isEmpty() && !password.isEmpty();
         }
 }
